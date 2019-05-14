647252b949448afc8ca615c626d705c5bdfcb65b
Grant Adams <adamsgd@uw.edu>
2019-05-14 10:24:09 -0700

    update runs

--- GOA/GOA_18.1.0/GOA_18.1.0.R
+++ GOA/GOA_18.1.0/GOA_18.1.0.R
@@ -4,8 +4,8 @@ library(Rceattle)
 # Data
 ################################################
 # Read the data in
-mydata <- Rceattle::read_excel( file = "GOA_18.1.0/GOA2017SS_v3_from_1977_v2.xlsx")
-
+mydata <- Rceattle::read_excel( file = "GOA_18.1.0/GOA2017SS_v3_from_1977_v2_fit_switch.xlsx")
+mydata$fsh_control$Nselages[15]
 
 ################################################
 # Estimation
@@ -18,27 +18,27 @@ ss_run <- Rceattle::fit_mod(data_list = mydata,
                             inits = inits, # Initial parameters = 0
                             file = NULL, # Don't save
                             debug = 0, # Estimate
-                            random_rec = TRUE, # No random recruitment
+                            random_rec = FALSE, # No random recruitment
                             msmMode = 0, # Single species mode
                             silent = FALSE)
 
 # ss_run$estimated_params$ln_mean_F
 #  ss_run$quantities$jnll_comp
-file_name <- "GOA_18.1.0/Figures/GOA_18.1.0"
-plot_index(ss_run, file = file_name)
-plot_catch(ss_run, file = file_name)
-Rceattle::plot_srv_comp(ss_run, file = file_name)
-Rceattle::plot_fsh_comp(ss_run, file = file_name)
-plot_biomass(ss_run, file = file_name)
-plot_recruitment(ss_run, file = file_name)
-plot_selectivity(ss_run, file = file_name)
+# file_name <- "GOA_18.1.0/Figures/GOA_18.1.0"
+# plot_index(ss_run, file = file_name)
+# plot_catch(ss_run, file = file_name)
+# Rceattle::plot_srv_comp(ss_run, file = file_name)
+# Rceattle::plot_fsh_comp(ss_run, file = file_name)
+# plot_biomass(ss_run, file = file_name)
+# plot_recruitment(ss_run, file = file_name)
+# plot_selectivity(ss_run, file = file_name)
 
 
 ms_run <- fit_mod(data_list = mydata,
                             inits = ss_run$estimated_params, # Initial parameters = 0
                             file = NULL, # Don't save
                             debug = 0, # Estimate
-                            random_rec = TRUE, # No random recruitment
+                            random_rec = FALSE, # No random recruitment
                             msmMode = 1, # Single species mode
                             silent = FALSE,
                             niter= 10)
--- GOA/GOA_18.1.0/GOA_18.1.2.R
+++ GOA/GOA_18.1.0/GOA_18.1.2.R
@@ -8,50 +8,79 @@ library(Rceattle)
 ################################################
 # Read the data in
 mydata <- Rceattle::read_excel( file = "GOA_18.1.0/GOA2017SS_v3_from_1977_v2_fit_switch.xlsx")
-
+mydata$fsh_control$Nselages[16]
 
 ################################################
 # Estimation
 ################################################
-inits <- build_params(mydata)
-inits$ln_mn_rec <- c(4,4,4)
+srv_fit <- list()
+ss_run <- list()
+ms_run <- list()
+
+run_mat <- matrix(NA, ncol = 2, nrow = 9)
+
+# Fit removing survey
+for(i in 1:6){
+  mydata2 <- mydata
+  mydata2$srv_control$Fit_0no_1yes[i] <- 0
+
+  
+  inits <- build_params(mydata2)
+  inits$ln_mn_rec <- c(4,4,4)
+  
+  
+  ss_run[[i]] <- Rceattle::fit_mod(data_list = mydata2,
+                              inits = inits, # Initial parameters = 0
+                              file = NULL, # Don't save
+                              debug = 0, # Estimate
+                              random_rec = FALSE, # No random recruitment
+                              msmMode = 0, # Single species mode
+                              silent = TRUE)
+  run_mat[i,1] <- sum(!is.na(ss_run[[i]]$sdrep$sd))
+  
+  
+  ms_run[[i]] <- Rceattle::fit_mod(data_list = mydata2,
+                              inits = ss_run[[i]]$estimated_params, # Initial parameters = 0
+                              file = NULL, # Don't save
+                              debug = 0, # Estimate
+                              random_rec = FALSE, # No random recruitment
+                              msmMode = 1, # Single species mode
+                              silent = TRUE,
+                              niter= 10)
+  run_mat[i,2] <- sum(!is.na(ms_run[[i]]$sdrep$sd))
+
+}
 
 
-ss_run <- Rceattle::fit_mod(data_list = mydata,
-                            inits = inits, # Initial parameters = 0
-                            file = NULL, # Don't save
-                            debug = 0, # Estimate
-                            random_rec = TRUE, # No random recruitment
-                            msmMode = 0, # Single species mode
-                            silent = FALSE)
-
-
-
-ms_run <- Rceattle::fit_mod(data_list = mydata,
-                  inits = ss_run$estimated_params, # Initial parameters = 0
-                  file = NULL, # Don't save
-                  debug = 0, # Estimate
-                  random_rec = TRUE, # No random recruitment
-                  msmMode = 1, # Single species mode
-                  silent = FALSE,
-                  niter= 10)
-# ss_run$estimated_params$ln_mean_F
-ms_run$quantities$jnll_comp
-ms_run$quantities$NByage[1,,]
-ms_run$quantities$ration2Age[2,,] # NA on year 3 age 1
-mydata$Pyrs[,,2]
-
-
-
-library(TMBhelper)
-identified <- suppressMessages(TMBhelper::Check_Identifiable(ms_run$obj))
-
-# Make into list
-identified_param_list <- ss_run$obj$env$parList(as.numeric(identified$BadParams$Param_check))
-identified_param_list <- rapply(identified_param_list,function(x) ifelse(x==0,"Not estimated",x), how = "replace")
-identified_param_list <- rapply(identified_param_list,function(x) ifelse(x==2,"OK",x), how = "replace")
-identified_param_list <- rapply(identified_param_list,function(x) ifelse(x==1,"BAD",x), how = "replace")
-
-identified_param_list
-
-ss_run$quantities$srv_q
+# Sub out fisheries
+fsh <- 2:4
+for(i in 1:length(fsh)){
+  mydata2 <- mydata
+  mydata2$fsh_control$Fit_0no_1yes[fsh[i]] <- 0
+  
+  
+  inits <- build_params(mydata2)
+  inits$ln_mn_rec <- c(4,4,4)
+  
+  
+  ss_run[[i+6]] <- Rceattle::fit_mod(data_list = mydata2,
+                                   inits = inits, # Initial parameters = 0
+                                   file = NULL, # Don't save
+                                   debug = 0, # Estimate
+                                   random_rec = FALSE, # No random recruitment
+                                   msmMode = 0, # Single species mode
+                                   silent = TRUE)
+  
+  run_mat[i+6,1] <- sum(!is.na( ss_run[[i+6]]$sdrep$sd))
+  
+  ms_run[[i+6]] <- Rceattle::fit_mod(data_list = mydata2,
+                                   inits = ss_run[[i+6]]$estimated_params, # Initial parameters = 0
+                                   file = NULL, # Don't save
+                                   debug = 0, # Estimate
+                                   random_rec = FALSE, # No random recruitment
+                                   msmMode = 1, # Single species mode
+                                   silent = TRUE,
+                                   niter= 10)
+  
+  run_mat[i+6,2] <- sum(!is.na(ms_run[[i+6]]$sdrep$sd))
+}
