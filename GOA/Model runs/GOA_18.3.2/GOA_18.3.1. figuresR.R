11ea16dd0f71e7df6015e036c758fe932bd9dda0
Grant Adams <adamsgd@uw.edu>
2020-10-09 12:04:45 -0700

    changing around plots and such

---
+++ GOA/Model runs/GOA_18.3.2/GOA_18.3.1. figuresR.R
@@ -0,0 +1,59 @@
+library(Rceattle)
+setwd("Model runs/GOA_18.3.2")
+load("Models/18_3_1.RData")
+
+# Re-order and name models
+# The long time-series models for 1977 to 2018 were: 
+#   •	Model 1: a model that did not include predation (single-species models) representing a base model. 
+# •	Model 2: a model that did not include halibut predation to allow comparisons in which halibut does not impact the dynamics of groundfish in the GOA. 
+# •	Models 3-5: models that included pre-specified mid-year numbers-at-age of Pacific halibut from the coastwide long-time (1917-2018) series model developed by the IPHC. To account for a lack of information on halibut distribution prior to 1993, numbers-at-age prior to 1993 were multiplied by the 50th (model 3), 15th (model 4), and 85th (model 5) quantiles of the distribution of adult halibut in area 3 between 1993 and 2018. 
+# •	Models 6-8: as for models 3-5 but using numbers-at-age of Pacific halibut from the areas-as-fleets long-time series model. 
+
+# The five short term models for 1993 to 2018 were: 
+#   •	Model 9: a model that does not include predation (model 9) to represent a base single-species model 
+# •	Model 10: a model that did not include halibut predation (model 10). 
+# •	Model 11: a model with pre-specified mid-year numbers-at-age of Pacific halibut from the coastwide short-time series model. 
+# •	Model 12: as for models 11but using numbers-at-age of Pacific halibut from the areas-as-fleets short-time series model 
+# •	Model 13: a model relative abundance-at-age of Pacific halibut in area 3 multiplied by an estimated parameter to allow the model to estimate the relative contribution of Pacific halibut predation to describing the dynamics of pollock, Pacific cod, and arrowtooth flounder. 
+
+
+
+mod_names_long <- c("1. SS", "2. MS-No Halibut", "3. MS-Coast avg", "4. MS-Coast low", "5. MS-Coast high", "6. MS-AAF avg", "7. MS-AAF low", "8. MS-AAF high")
+mod_names_short <- c("9. SS", "10. MS-No Halibut", "11. MS-Coast", "12. MS-AAF", "13. MS-Survey")
+mod_list_long <- mod_list_all[1:8]
+mod_list_short <- mod_list_all[9:13]
+mod_names_all <- c(mod_names_long, mod_names_short)
+
+file_name <- "Figures/18.3.1/18.3.1_models_long"
+plot_biomass(mod_list_long, file = file_name, model_names = mod_names_long, right_adj = 9)
+plot_ssb(mod_list_long, file = file_name, model_names = mod_names_long, right_adj = 9)
+plot_recruitment(mod_list_long, file = file_name, add_ci = TRUE, model_names = mod_names_long, right_adj = 9)
+plot_index(mod_list_long, file = file_name, model_names = mod_names_long, right_adj = 9)
+plot_logindex(mod_list_long, file = file_name, model_names = mod_names_long, right_adj = 0.25, top_adj = 0.1)
+plot_catch(mod_list_long, file = file_name, model_names = mod_names_long, right_adj = 0.25, top_adj = 0.1)
+
+nll_long <- data.frame(nll = sapply(mod_list_long, function(x) x$opt$objective),
+                       aic = sapply(mod_list_long, function(x) x$opt$AIC))
+nll_long$daic <- nll_long$aic - min(nll_long$aic)
+write.csv(nll_long, "Figures/18.3.1/18.3.1.long_model_nll.csv")
+
+# Short
+file_name <- "Figures/18.3.1/18.3.1_models_short"
+plot_biomass(mod_list_short, file = file_name, model_names = mod_names_short, right_adj = 5.5)
+plot_ssb(mod_list_short, file = file_name, model_names = mod_names_short, right_adj = 5.5)
+plot_recruitment(mod_list_short, file = file_name, add_ci = TRUE, model_names = mod_names_short, right_adj = 5.5)
+plot_index(mod_list_short, file = file_name, model_names = mod_names_short, right_adj = 5.5)
+plot_logindex(mod_list_short, file = file_name, model_names = mod_names_short, right_adj = 0.1)
+plot_catch(mod_list_short, file = file_name, model_names = mod_names_short, right_adj = 0.1)
+nll_short <- data.frame(nll = sapply(mod_list_short, function(x) x$opt$objective),
+                        aic = sapply(mod_list_short, function(x) x$opt$AIC))
+nll_short$daic <- nll_short$aic - min(nll_short$aic)
+
+write.csv(nll_short, "Figures/18.3.1/18.3.1.short_model_nll.csv")
+
+
+file_name <- "Figures/18.3.1/18.3.1_models_all"
+plot_biomass(mod_list_all, file = file_name, model_names = mod_names_all, right_adj = 9)
+plot_ssb(mod_list_all, file = file_name, model_names = mod_names_all, right_adj = 9)
+plot_recruitment(mod_list_all, file = file_name, add_ci = FALSE, model_names = mod_names_all, right_adj = 9)
+
