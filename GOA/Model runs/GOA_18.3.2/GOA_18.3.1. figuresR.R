5c10573c0e48812575363c4dab6f9b94aa8fb8ef
Grant Adams <adamsgd@uw.edu>
2020-10-20 13:04:09 -0700

    update figures

--- GOA/Model runs/GOA_18.3.2/GOA_18.3.1. figuresR.R
+++ GOA/Model runs/GOA_18.3.2/GOA_18.3.1. figuresR.R
@@ -1,6 +1,11 @@
 library(Rceattle)
+library(wesanderson)
 setwd("Model runs/GOA_18.3.2")
 load("Models/18_3_1.RData")
+
+mod_list_all <- mod_list_all[1:13]
+sapply(mod_list_all, function(x) x$opt$objective) - sapply(mod_list_all, function(x) x$quantities$jnll)
+sapply(mod_list_all, function(x) x$sdrep$sd[1])
 
 # Re-order and name models
 # The long time-series models for 1977 to 2018 were: 
@@ -24,14 +29,17 @@ ms_mod_list <- mod_list_all[c(2:8, 10:13)]
 mod_list_long <- mod_list_all[1:8]
 mod_list_short <- mod_list_all[9:13]
 mod_names_all <- c(mod_names_long, mod_names_short)
+line_col <- oce::oce.colorsViridis(13)[1:13] #tableau_color_pal(palette = "Hue Circle", type = c("regular", "ordered-sequential", "ordered-diverging"), direction = 1)(19)[c(1:8, 9,11,13,15,19)] # c(wes_palette("Darjeeling1", 8, type = "continuous"), wes_palette("Darjeeling2", 5, type = "continuous"))
+line_col_long <- line_col[1:8]
+line_col_short <- line_col[9:13]
 
 file_name <- "Figures/18.3.1/18.3.1_models_long"
-plot_biomass(mod_list_long, file = file_name, model_names = mod_names_long, right_adj = 9)
-plot_ssb(mod_list_long, file = file_name, model_names = mod_names_long, right_adj = 9)
-plot_recruitment(mod_list_long, file = file_name, add_ci = TRUE, model_names = mod_names_long, right_adj = 9)
-plot_index(mod_list_long, file = file_name, model_names = mod_names_long, right_adj = 9)
-plot_logindex(mod_list_long, file = file_name, model_names = mod_names_long, right_adj = 0.25, top_adj = 0.1)
-plot_catch(mod_list_long, file = file_name, model_names = mod_names_long, right_adj = 0.25, top_adj = 0.1)
+plot_biomass(mod_list_long, file = file_name, model_names = mod_names_long, right_adj = 0.27, line_col = line_col_long)
+plot_ssb(mod_list_long, file = file_name, model_names = mod_names_long, right_adj = 0.27, line_col = line_col_long)
+plot_recruitment(mod_list_long, file = file_name, add_ci = TRUE, model_names = mod_names_long, right_adj = 0.27, line_col = line_col_long)
+plot_index(mod_list_long, file = file_name, model_names = mod_names_long, right_adj = 0, line_col = line_col_long)
+plot_logindex(mod_list_long, file = file_name, model_names = mod_names_long, right_adj = 0.25, top_adj = 0.27, line_col = line_col_long)
+plot_catch(mod_list_long, file = file_name, model_names = mod_names_long, right_adj = 0.25, top_adj = 0, line_col = line_col_long)
 
 nll_long <- data.frame(nll = sapply(mod_list_long, function(x) x$opt$objective),
                        aic = sapply(mod_list_long, function(x) x$opt$AIC))
@@ -40,21 +48,80 @@ write.csv(nll_long, "Figures/18.3.1/18.3.1.long_model_nll.csv")
 
 # Short
 file_name <- "Figures/18.3.1/18.3.1_models_short"
-plot_biomass(mod_list_short, file = file_name, model_names = mod_names_short, right_adj = 5.5)
-plot_ssb(mod_list_short, file = file_name, model_names = mod_names_short, right_adj = 5.5)
-plot_recruitment(mod_list_short, file = file_name, add_ci = TRUE, model_names = mod_names_short, right_adj = 5.5)
-plot_index(mod_list_short, file = file_name, model_names = mod_names_short, right_adj = 5.5)
-plot_logindex(mod_list_short, file = file_name, model_names = mod_names_short, right_adj = 0.1)
-plot_catch(mod_list_short, file = file_name, model_names = mod_names_short, right_adj = 0.1)
+plot_biomass(mod_list_short, file = file_name, model_names = mod_names_short, right_adj = 0.17, line_col = line_col_short)
+plot_ssb(mod_list_short, file = file_name, model_names = mod_names_short, right_adj = 0.17, line_col = line_col_short)
+plot_recruitment(mod_list_short, file = file_name, add_ci = TRUE, model_names = mod_names_short, right_adj = 0.17, line_col = line_col_short)
+plot_index(mod_list_short, file = file_name, model_names = mod_names_short, right_adj = 0, line_col = line_col_short)
+plot_logindex(mod_list_short, file = file_name, model_names = mod_names_short, right_adj = 0.1, line_col = line_col_short)
+plot_catch(mod_list_short, file = file_name, model_names = mod_names_short, right_adj = 0, line_col = line_col_short)
 nll_short <- data.frame(nll = sapply(mod_list_short, function(x) x$opt$objective),
                         aic = sapply(mod_list_short, function(x) x$opt$AIC))
 nll_short$daic <- nll_short$aic - min(nll_short$aic)
 
 write.csv(nll_short, "Figures/18.3.1/18.3.1.short_model_nll.csv")
 
-
 file_name <- "Figures/18.3.1/18.3.1_models_all"
-plot_biomass(mod_list_all, file = file_name, model_names = mod_names_all, right_adj = 9)
-plot_ssb(mod_list_all, file = file_name, model_names = mod_names_all, right_adj = 9)
-plot_recruitment(mod_list_all, file = file_name, add_ci = FALSE, model_names = mod_names_all, right_adj = 9)
+plot_biomass(mod_list_all, file = file_name, model_names = mod_names_all, right_adj = 0.17, line_col = line_col, lwd = 2)
+plot_ssb(mod_list_all, file = file_name, add_ci = FALSE, model_names = mod_names_all, right_adj = 0.17, line_col = line_col, lwd = 2)
+plot_recruitment(mod_list_all, file = file_name, add_ci = FALSE, model_names = mod_names_all, right_adj = 0.17, line_col = line_col, lwd = 2)
+plot_logindex(mod_list_all, file = file_name, model_names = mod_names_all, right_adj = 0, line_col = line_col)
 
+
+# Table of likelihood components
+jnll_summary <- data.frame(matrix(NA, nrow = nrow(mod_list_all[[1]]$quantities$jnll_comp), ncol = (length(mod_list_all)+1)))
+jnll_summary[,1] = rownames(mod_list_all[[1]]$quantities$jnll_comp)
+
+for(i in 1:length(mod_list_all)){
+  jnll_summary[,i+1] <- rowSums(mod_list_all[[i]]$quantities$jnll_comp)
+  if(i < 9){
+    jnll_summary[12,i+1] = jnll_summary[12,i+1] - (999^2 * 10)
+  }
+}
+jnll_summary <- rbind(jnll_summary, c("k", sapply(mod_list_all, function(x) x$opt$number_of_coefficients[1])))
+write.csv(jnll_summary, "Figures/18.3.1/18.3.1.long_model_jnll_summary.csv")
+
+
+# Table of likelihood components
+jnll_summary <- data.frame(matrix(NA, nrow = nrow(mod_list_all[[1]]$quantities$jnll_comp), ncol = (length(mod_list_all)+1) * 3))
+jnll_summary[,1] = rownames(mod_list_all[[1]]$quantities$jnll_comp)
+
+for(i in 1:length(mod_list_all)){
+  jnll_summary[,i+1] <- rowSums(mod_list_all[[i]]$quantities$jnll_comp)
+  if(i < 9){
+    jnll_summary[12,i+1] = jnll_summary[12,i+1] - (999^2 * 10)
+  }
+}
+jnll_summary <- rbind(jnll_summary, c("k", sapply(mod_list_all, function(x) x$opt$number_of_coefficients[1])))
+write.csv(jnll_summary, "Figures/18.3.1/18.3.1.long_model_jnll_summary.csv")
+
+
+
+
+# Difference in biomass
+ssb_diff_long <- lapply(mod_list_all[2:8], function(x) x$quantities$biomassSSB / mod_list_all[[1]]$quantities$biomassSSB)
+biom_diff_long <- lapply(mod_list_all[2:8], function(x) x$quantities$biomass / mod_list_all[[1]]$quantities$biomass)
+R_diff_long <- lapply(mod_list_all[2:8], function(x) x$quantities$R / mod_list_all[[1]]$quantities$R)
+
+mean(sapply(ssb_diff_long[1:7], function(x) mean(x[1,])))
+mean(sapply(biom_diff_long[1:7], function(x) mean(x[1,])))
+mean(sapply(R_diff_long[1:7], function(x) mean(x[1,])))
+
+mean(ssb_diff_long[[1]][1,])
+mean(biom_diff_long[[1]][1,])
+sapply(ssb_diff_long[2:7], function(x) mean(x[1,]))
+sapply(biom_diff_long[2:7], function(x) mean(x[1,]))
+
+ssb_diff_short <- lapply(mod_list_all[10:13], function(x) x$quantities$biomassSSB / mod_list_all[[9]]$quantities$biomassSSB)
+biom_diff_short <- lapply(mod_list_all[10:13], function(x) x$quantities$biomass / mod_list_all[[9]]$quantities$biomass)
+R_diff_short <- lapply(mod_list_all[10:13], function(x) x$quantities$R / mod_list_all[[9]]$quantities$R)
+
+
+mean(sapply(ssb_diff_short[1:4], function(x) mean(x[1,])))
+mean(sapply(biom_diff_short[1:4], function(x) mean(x[1,])))
+mean(sapply(R_diff_short[1:4], function(x) mean(x[1,])))
+
+
+mean(ssb_diff_short[[1]][1,])
+mean(biom_diff_short[[1]][1,])
+sapply(ssb_diff_short[2:4], function(x) mean(x[1,]))
+sapply(biom_diff_short[2:4], function(x) mean(x[1,]))
