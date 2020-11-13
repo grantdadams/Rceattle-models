ed5747b1de3f6787afec6367c5b67e998b80ac15
Grant Adams <adamsgd@uw.edu>
2020-11-13 14:02:21 -0800

    adding n at age comparison

--- GOA/Model runs/GOA_18.5.1/GOA_18.5.1.pollock.check.R
+++ GOA/Model runs/GOA_18.5.1/GOA_18.5.1.pollock.check.R
@@ -9,14 +9,14 @@ mydata_pollock <- Rceattle::read_data( file = "Data/GOA_18.5.1_pollock_single_sp
 mydata_pollock2 <- Rceattle::read_data( file = "Data/GOA_18.5.1_pollock_single_species_1970-2018_fixed_bt_sel.xlsx")
 
 pollock_base <- Rceattle::fit_mod(data_list = mydata_pollock,
-                  inits = NULL, # Initial parameters = 0
-                  file = NULL, # Don't save
-                  debug = 0, # Estimate
-                  random_rec = FALSE, # No random recruitment
-                  msmMode = 0, # Single species mode
-                  silent = TRUE,
-                  recompile = FALSE,
-                  phase = "default")
+                                  inits = NULL, # Initial parameters = 0
+                                  file = NULL, # Don't save
+                                  debug = 0, # Estimate
+                                  random_rec = FALSE, # No random recruitment
+                                  msmMode = 0, # Single species mode
+                                  silent = TRUE,
+                                  recompile = FALSE,
+                                  phase = "default")
 
 # Random walk of q - adfg, shelikof, bt, random walk of fishery sel
 mydata_pollock_rw <- mydata_pollock
@@ -28,19 +28,6 @@ mydata_pollock_rw$fleet_control$Sel_sd_prior[8] <- 0.05
 mydata_pollock_rw$fleet_control$Selectivity[c(8)] <- 3
 
 pollock_rw <- Rceattle::fit_mod(data_list = mydata_pollock_rw,
-                                  inits = NULL, # Initial parameters = 0
-                                  file = NULL, # Don't save
-                                  debug = 0, # Estimate
-                                  random_rec = FALSE, # No random recruitment
-                                  msmMode = 0, # Single species mode
-                                  silent = TRUE,
-                                  recompile = FALSE,
-                                  phase = "default")
-
-mydata_pollock_fq <- mydata_pollock_rw
-mydata_pollock_fq$fleet_control$Estimate_q[2] <- 0
-mydata_pollock_fq$fleet_control$Log_q_prior <- log(0.85)
-pollock_fix_q <- Rceattle::fit_mod(data_list = mydata_pollock_fq,
                                 inits = NULL, # Initial parameters = 0
                                 file = NULL, # Don't save
                                 debug = 0, # Estimate
@@ -49,6 +36,19 @@ pollock_fix_q <- Rceattle::fit_mod(data_list = mydata_pollock_fq,
                                 silent = TRUE,
                                 recompile = FALSE,
                                 phase = "default")
+
+mydata_pollock_fq <- mydata_pollock_rw
+mydata_pollock_fq$fleet_control$Estimate_q[2] <- 0
+mydata_pollock_fq$fleet_control$Log_q_prior <- log(0.85)
+pollock_fix_q <- Rceattle::fit_mod(data_list = mydata_pollock_fq,
+                                   inits = NULL, # Initial parameters = 0
+                                   file = NULL, # Don't save
+                                   debug = 0, # Estimate
+                                   random_rec = FALSE, # No random recruitment
+                                   msmMode = 0, # Single species mode
+                                   silent = TRUE,
+                                   recompile = FALSE,
+                                   phase = "default")
 
 
 # SAFE Models
@@ -70,8 +70,8 @@ pollock_fix_q$quantities$biomass[1,1:49] <- colSums(pollock_fix_q$quantities$bio
 ######################### Plots
 # - SAFE vs SS
 file_name <- "Figures/18.5.1/18.5.1_SAFE_vs_ceattle_pollock"
-mod_list <- list(pollock_base, pollock_safe)
-mod_names <- c("CEATTLE pollock", "2018 SAFE (mt)")
+mod_list <- list(pollock_base, pollock_rw, pollock_safe)
+mod_names <- c("CEATTLE pollock", "CEATTLE pollock rw", "2018 SAFE (mt)")
 
 plot_biomass(mod_list, file = file_name, model_names = mod_names, right_adj = 0.27, line_col = NULL, species = 1)
 plot_ssb(mod_list, file = file_name, model_names = mod_names, right_adj = 0.27, line_col = NULL, species = 1)
@@ -79,6 +79,15 @@ plot_recruitment(mod_list, file = file_name, add_ci = FALSE, model_names = mod_n
 
 
 
-# rec plots 
-plot(y = safe_nage$Age1, x = 1970:2018, type = "l")
-lines(y = pollock_base$quantities$NByage[1,1,1,1:49]/1000000, x = 1970:2018, lty = 2)
+# rec plots
+jpeg(filename = "Figures/natage.compare.jpg", height = 1, width = 8, res = 300, units = "in")
+par(mfrow = c(5,2), mar = c(2.8,3.8,1,1))
+for(i in 1:10){
+  safe = safe_nage[[paste0("Age",i)]]
+  ceattle <- pollock_base$quantities$NByage[1,1,i,1:49]/1000000
+  plot(y = safe, x = 1970:2018, type = "l", xlab = "Year", ylab = "N (millions)", ylim = c(0, max(c(safe, ceattle))))
+  lines(y = ceattle, x = 1970:2018, lty = 2)
+  legend("topleft", c("SAFE", "CEATTLE"), lty = c(1,2), bty = "n")
+  legend("top", paste0("Age ",i), bty = "n")
+}
+dev.off()
