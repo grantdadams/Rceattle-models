359038aedb40d20b64115305ba420544e3507b63
Grant Adams <adamsgd@uw.edu>
2020-11-13 11:31:57 -0800

    updated figures

--- GOA/Model runs/GOA_18.5.1/GOA_18.5.1.pollock.check.R
+++ GOA/Model runs/GOA_18.5.1/GOA_18.5.1.pollock.check.R
@@ -35,25 +35,41 @@ pollock_rw <- Rceattle::fit_mod(data_list = mydata_pollock_rw,
                                   recompile = FALSE,
                                   phase = "default")
 
+mydata_pollock_fq <- mydata_pollock_rw
+mydata_pollock_fq$fleet_control$Estimate_q[2] <- 0
+mydata_pollock_fq$fleet_control$Log_q_prior <- log(0.85)
+pollock_fix_q <- Rceattle::fit_mod(data_list = mydata_pollock_fq,
+                                inits = NULL, # Initial parameters = 0
+                                file = NULL, # Don't save
+                                debug = 0, # Estimate
+                                random_rec = FALSE, # No random recruitment
+                                msmMode = 0, # Single species mode
+                                silent = TRUE,
+                                recompile = FALSE,
+                                phase = "default")
+
+
 # SAFE Models
 library(readxl)
 safe2018biomass <- as.data.frame(read_xlsx("Data/2018_SAFE_pollock_estimates.xlsx", sheet = 1))
 safe2018ssb <- as.data.frame(read_xlsx("Data/2018_SAFE_pollock_estimates.xlsx", sheet = 2))
 safe2018rec <- as.data.frame(read_xlsx("Data/2018_SAFE_pollock_estimates.xlsx", sheet = 3))
 pollock_safe <- pollock_base
-pollock_safe$quantities$biomass[1,1:49] <- t(safe2018biomass[,2]) * 1000000 * 0.453592
-pollock_safe$quantities$biomassSSB[1,1:49] <- t(safe2018ssb[,2])  * 1000000 * 0.453592
+pollock_safe$quantities$biomass[1,1:49] <- t(safe2018biomass[,2]) * 1000000
+pollock_safe$quantities$biomassSSB[1,1:49] <- t(safe2018ssb[,2])  * 1000000 
 pollock_safe$quantities$R[1,1:49] <- t(safe2018rec[,2]) * 1000000
 
 
-pollock_base$quantities$biomass[1,1:49] <- colSums(pollock_base$quantities$biomassByage[1,3:10,1:49])
-pollock_rw$quantities$biomass[1,1:49] <- colSums(pollock_rw$quantities$biomassByage[1,3:10,1:49])
+pollock_base$quantities$biomass[1,1:49] <- colSums(pollock_base$quantities$biomassByage[1,3:10,1:49]) * 2
+pollock_rw$quantities$biomass[1,1:49] <- colSums(pollock_rw$quantities$biomassByage[1,3:10,1:49]) * 2
+pollock_fix_q$quantities$biomass[1,1:49] <- colSums(pollock_fix_q$quantities$biomassByage[1,3:10,1:49]) * 2
 
 ######################### Plots
 # - SAFE vs SS
 file_name <- "Figures/18.5.1/18.5.1_SAFE_vs_ceattle_pollock"
-mod_list <- list(pollock_base, pollock_rw, pollock_safe)
-mod_names <- c("CEATTLE pollock", "CEATTLE pollock rw", "2018 SAFE (mt)")
+mod_list <- list(pollock_base, pollock_rw, pollock_fix_q, pollock_safe)
+mod_names <- c("CEATTLE pollock", "CEATTLE pollock rw", "CEATTLE fix BT q", "2018 SAFE (mt)")
+for(i in 1:3){mod_list[[i]]$quantities$biomassSSB = mod_list[[i]]$quantities$biomassSSB * 2}
 
 plot_biomass(mod_list, file = file_name, model_names = mod_names, right_adj = 0.27, line_col = NULL, species = 1)
 plot_ssb(mod_list, file = file_name, model_names = mod_names, right_adj = 0.27, line_col = NULL, species = 1)
