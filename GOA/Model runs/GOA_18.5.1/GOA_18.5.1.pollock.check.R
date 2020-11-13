ddf533bac1424bfe72edbd0dffb5b9ebd18de302
Grant Adams <adamsgd@uw.edu>
2020-11-13 13:42:33 -0800

    adding safe comparison

--- GOA/Model runs/GOA_18.5.1/GOA_18.5.1.pollock.check.R
+++ GOA/Model runs/GOA_18.5.1/GOA_18.5.1.pollock.check.R
@@ -6,8 +6,9 @@ setwd("Model runs/GOA_18.5.1/")
 ################################################
 # Read the data in
 mydata_pollock <- Rceattle::read_data( file = "Data/GOA_18.5.1_pollock_single_species_1970-2018.xlsx")
+mydata_pollock2 <- Rceattle::read_data( file = "Data/GOA_18.5.1_pollock_single_species_1970-2018_fixed_bt_sel.xlsx")
 
-pollock_base <- Rceattle::fit_mod(data_list = mydata_pollock,
+pollock_base <- Rceattle::fit_mod(data_list = mydata_pollock2,
                   inits = NULL, # Initial parameters = 0
                   file = NULL, # Don't save
                   debug = 0, # Estimate
@@ -18,13 +19,14 @@ pollock_base <- Rceattle::fit_mod(data_list = mydata_pollock,
                   phase = "default")
 
 # Random walk of q - adfg, shelikof, bt, random walk of fishery sel
-mydata_pollock_rw <- mydata_pollock
+mydata_pollock_rw <- mydata_pollock2
 mydata_pollock_rw$fleet_control$Time_varying_q[c(1,2,3,7)] <- 1
 mydata_pollock_rw$fleet_control$Q_sd_prior[c(1,2,3,7)] <- 0.05
 
 mydata_pollock_rw$fleet_control$Time_varying_sel[8] <- 1
 mydata_pollock_rw$fleet_control$Sel_sd_prior[8] <- 0.05
-mydata_pollock_rw$fleet_control$Selectivity[8] <- 3
+mydata_pollock_rw$fleet_control$Selectivity[c(8)] <- 3
+
 pollock_rw <- Rceattle::fit_mod(data_list = mydata_pollock_rw,
                                   inits = NULL, # Initial parameters = 0
                                   file = NULL, # Don't save
@@ -34,6 +36,7 @@ pollock_rw <- Rceattle::fit_mod(data_list = mydata_pollock_rw,
                                   silent = TRUE,
                                   recompile = FALSE,
                                   phase = "default")
+sum(pollock_rw$quantities$jnll_comp)
 
 mydata_pollock_fq <- mydata_pollock_rw
 mydata_pollock_fq$fleet_control$Estimate_q[2] <- 0
@@ -60,16 +63,15 @@ pollock_safe$quantities$biomassSSB[1,1:49] <- t(safe2018ssb[,2])  * 1000000
 pollock_safe$quantities$R[1,1:49] <- t(safe2018rec[,2]) * 1000000
 
 
-pollock_base$quantities$biomass[1,1:49] <- colSums(pollock_base$quantities$biomassByage[1,3:10,1:49]) * 2
-pollock_rw$quantities$biomass[1,1:49] <- colSums(pollock_rw$quantities$biomassByage[1,3:10,1:49]) * 2
-pollock_fix_q$quantities$biomass[1,1:49] <- colSums(pollock_fix_q$quantities$biomassByage[1,3:10,1:49]) * 2
+pollock_base$quantities$biomass[1,1:49] <- colSums(pollock_base$quantities$biomassByage[1,3:10,1:49])
+pollock_rw$quantities$biomass[1,1:49] <- colSums(pollock_rw$quantities$biomassByage[1,3:10,1:49])
+pollock_fix_q$quantities$biomass[1,1:49] <- colSums(pollock_fix_q$quantities$biomassByage[1,3:10,1:49])
 
 ######################### Plots
 # - SAFE vs SS
 file_name <- "Figures/18.5.1/18.5.1_SAFE_vs_ceattle_pollock"
 mod_list <- list(pollock_base, pollock_rw, pollock_fix_q, pollock_safe)
-mod_names <- c("CEATTLE pollock", "CEATTLE pollock rw", "CEATTLE fix BT q", "2018 SAFE (mt)")
-for(i in 1:3){mod_list[[i]]$quantities$biomassSSB = mod_list[[i]]$quantities$biomassSSB * 2}
+mod_names <- c("CEATTLE pollock", "CEATTLE pollock rw", "CEATTLE fix BT q 0.85", "2018 SAFE (mt)")
 
 plot_biomass(mod_list, file = file_name, model_names = mod_names, right_adj = 0.27, line_col = NULL, species = 1)
 plot_ssb(mod_list, file = file_name, model_names = mod_names, right_adj = 0.27, line_col = NULL, species = 1)
