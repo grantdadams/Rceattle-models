9180aedbfa581682bbb316e8b33cc0a56c381e32
Grant Adams <adamsgd@uw.edu>
2025-04-02 14:05:00 -0700

    add ebs

--- EBS pollock/Run CEATTLE.R
+++ EBS pollock/Run CEATTLE.R
@@ -5,7 +5,9 @@
 # - Fishery catch
 # - Fishery age composition
 # - Fishery weight-at-age
-# - Survey biomass and standard error
+# - Surveys
+# -- Bottom trawl (random walk-logistic for age > 1, normal deviates for age = 1), additional penalty on selectivity
+# -- AT (age-1 is an index, age > 1 have selectivity smoother)
 # - Bottom temperature
 # - Survey age composition
 # - Catch-at-age methodology
@@ -13,23 +15,21 @@
 # - Age at maturity
 
 # MODEL
-# - Two sex
-# - Survey selectivity = sex-combined logistic
-# - Survey q = q * e^(B*Env Indices)
-# - Fishery selectivity = sex-specific time-varying logistic
-# - Ricker recruitment (1978-2017)
+# - One sex
+# - Ricker recruitment (1978-2017) w/ prior on steepness
 # - Empirical weight-at-age
-# - M = 0.12 for females, estimated for males
+# - M = 0.3 for females, estimated for males
 
 # Load data ----
-library(Rceattle)
-mydata_yfs <- Rceattle::read_data( file = "Data/EBS_2024_pollock_single_species.xlsx")
-mydata_yfs$estDynamics = 0
-mydata_yfs$srv_biom$Log_sd <- mydata_yfs$srv_biom$Log_sd/mydata_yfs$srv_biom$Observation
-mydata_yfs$fsh_biom$Catch <- mydata_yfs$fsh_biom$Catch*1000
+library(Rceattle) # https://github.com/grantdadams/Rceattle/tree/dev
+ebs_pollock <- Rceattle::read_data( file = "Data/EBS_2024_pollock_single_species.xlsx")
+ebs_pollock$estDynamics = 0
+ebs_pollock$index_data$Log_sd <- ebs_pollock$index_data$Log_sd/ebs_pollock$index_data$Observation
+ebs_pollock$catch_data$Catch <- ebs_pollock$catch_data$Catch*1000
+ebs_pollock$catch_data$Log_sd <- 0.05
 
 # - Fix M
-bridging_model_1 <- Rceattle::fit_mod(data_list = mydata_yfs,
+bridging_model_1 <- Rceattle::fit_mod(data_list = ebs_pollock,
                                           inits = NULL, # Initial parameters = 0
                                           file = NULL, # Don't save
                                           estimateMode = 0, # Estimate
@@ -38,55 +38,7 @@ bridging_model_1 <- Rceattle::fit_mod(data_list = mydata_yfs,
                                           verbose = 2,
                                           phase = TRUE,
                                           initMode = 2)
-bridging_model_1$quantities$jnll_comp
-bridging_model_1$quantities$index_hat
 
-# - Est female and male M
-bridging_model_2 <- Rceattle::fit_mod(data_list = mydata_yfs,
-                                      inits = bridging_model_1$estimated_params,
-                                      file = NULL, # Don't save
-                                      estimateMode = 0, # Estimate
-                                      random_rec = FALSE, # No random recruitment
-                                      msmMode = 0, # Single species mode
-                                      verbose = 1,
-                                      M1Fun = build_M1(M1_model = c(2)),
-                                      phase = NULL,
-                                      initMode = 2)
-
-# - Fix female M and estimate male M
-inits <- bridging_model_1$estimated_params
-map <- build_map(data_list = bridging_model_2$data_list, params = inits)
-map$mapList$ln_M1[1,1,] <- NA
-map$mapFactor$ln_M1 <- factor(map$mapList$ln_M1)
-
-# -- Fix selectivity for survey to be sex-invariant
-map$mapList$sel_inf[1,1,] <- 1
-map$mapList$ln_sel_slp[1,1,] <- 1
-
-map$mapFactor$sel_inf <- factor(map$mapList$sel_inf)
-map$mapFactor$ln_sel_slp <- factor(map$mapList$ln_sel_slp)
-
-bridging_model_3 <- Rceattle::fit_mod(data_list = mydata_yfs,
-                                      inits = inits,
-                                      map = map,
-                                      file = NULL, # Don't save
-                                      estimateMode = 0, # Estimate
-                                      random_rec = FALSE, # No random recruitment
-                                      msmMode = 0, # Single species mode
-                                      verbose = 1,
-                                      phase = NULL,
-                                      initMode = 2)
-
-#
-# bridging_model_re <- Rceattle::fit_mod(data_list = mydata_yfs,
-#                                       inits = bridging_model_1$estimated_params, # Initial parameters = 0
-#                                       file = NULL, # Don't save
-#                                       estimateMode = 0, # Estimate
-#                                       random_rec = TRUE, # No random recruitment
-#                                       msmMode = 0, # Single species mode
-#                                       verbose = 1,
-#                                       phase = NULL,
-#                                       initMode = 2)
 
 
 # - SAFE model
