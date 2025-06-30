415ee795461de95bc29e157acc479f811ce3bce6
Grant.Adams <grantadams60091@gmail.com>
2025-06-30 13:52:22 -0700

    Update and clean EBS pollock repo

--- EBS pollock/Rceattle install.R
+++ EBS pollock/Rceattle install.R
@@ -20,4 +20,4 @@ pacman::p_load(dplyr,
                knitr,
                doParallel)
 devtools::install_github("kaskr/TMB_contrib_R/TMBhelper")
-remotes::install_github("grantdadams/Rceattle", ref = "dev") # dev branch is most up to date and STABLE branch
+remotes::install_github("grantdadams/Rceattle", ref = "dev-name-change")
